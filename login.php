9418755e33753313daa7cb902752a9f197c4dc6d
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-09-28 14:07:30 +0300

    m

--- login.php
+++ login.php
@@ -1,86 +1,56 @@
 <?php
-	
-	//loome andmebaasiühenduse
-	require_once("../config.php");
-	$database = "if15_anniant";
-	$mysqli = new mysqli($servername, $username, $password, $database);
-	
-	//check connection
-	if($mysqli->connect_error) {
-		die("connect_error ".mysqli_connect_error());
-	}
-
+    require_once("functions.php");
+    
+    //kui kasutaja on sisse logitud, suuna teisele lehele
+    //kontrollin kas sessiooni muutuja olemas
+    if(isset($_SESSION['logged_in_user_id'])){
+        header("Location: data.php");
+    }
   // muuutujad errorite jaoks
 	$email_error = "";
 	$password_error = "";
 	$create_email_error = "";
 	$create_password_error = "";
-
   // muutujad väärtuste jaoks
 	$email = "";
 	$password = "";
 	$create_email = "";
 	$create_password = "";
-
-
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
-
     // *********************
     // **** LOGI SISSE *****
     // *********************
 		if(isset($_POST["login"])){
-
 			if ( empty($_POST["email"]) ) {
 				$email_error = "See väli on kohustuslik";
 			}else{
         // puhastame muutuja võimalikest üleliigsetest sümbolitest
 				$email = cleanInput($_POST["email"]);
 			}
-
 			if ( empty($_POST["password"]) ) {
 				$password_error = "See väli on kohustuslik";
 			}else{
 				$password = cleanInput($_POST["password"]);
 			}
-
       // Kui oleme siia jõudnud, võime kasutaja sisse logida
 			if($password_error == "" && $email_error == ""){
-				echo "Võib sisse logida! Kasutajanimi on ".$email." ja parool on ".$password;
-				
-				$hash=hash("sha512", $password);
-				$stmt=$mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
-				
-				//küsimärkide asendus
-				$stmt->bind.param("ss", $email, $hash);
-				//andmebaasist tulnud muutujad
-				$stmt->bind.result($id_from_db, $email_from_db);
-				$stmt->execute();
-				
-				//teeb päringu ja kui on tõene st et andmebaasis oli see väärtus, siis me saame otsustada mis edasi teeme
-				if($stmt->fetch()) {
-					
-						//kasutaja email ja parool õiged
-						echo "Kasutaja logis sisse ".$id_from_db;
-				}else{
-					echo "Wrong credentials";
-				}
-				
-				$stmt->close();
-			}
-
+				//echo "Võib sisse logida! Kasutajanimi on ".$email." ja parool on ".$password;
+			
+                $hash = hash("sha512", $password);
+                
+                loginUser($email, $hash);
+            
+            }
 		} // login if end
-
     // *********************
     // ** LOO KASUTAJA *****
     // *********************
     if(isset($_POST["create"])){
-
 			if ( empty($_POST["create_email"]) ) {
 				$create_email_error = "See väli on kohustuslik";
 			}else{
 				$create_email = cleanInput($_POST["create_email"]);
 			}
-
 			if ( empty($_POST["create_password"]) ) {
 				$create_password_error = "See väli on kohustuslik";
 			} else {
@@ -90,31 +60,20 @@
 					$create_password = cleanInput($_POST["create_password"]);
 				}
 			}
-		
 			if(	$create_email_error == "" && $create_password_error == ""){
-				echo hash("sha512", $create_password);
-				echo "Võib kasutajat luua! Kasutajanimi on ".$create_email." ja parool on ".$create_password;
-				
-				//tekitan parooliräsi muutujasse hash
-				$hash= hash("sha512", $create_password);
-				
-				//salvestan andmebaasi
-				$stmt=$mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
-				
-				//paneme muutujad küsimärkide asemele
-				// ss= string, iga muutuja kohta 1 täht
-				$stmt->bind_param("ss", $create_email, $hash);
-				
-				//käivitab sisestuse
-				$stmt->execute();
-				$stmt->close();
-				
-			}
-
-    } // create if end
-
+				//echo hash("sha512", $create_password);
+                //echo "Võib kasutajat luua! Kasutajanimi on ".$create_email." ja parool on ".$create_password;
+                
+                // tekitan parooliräsi
+                $hash = hash("sha512", $create_password);
+                
+                //functions.php's funktsioon
+                createUser($create_email, $hash);
+                
+                
+            }
+        } // create if end
 	}
-
   // funktsioon, mis eemaldab kõikvõimaliku üleliigse tekstist
   function cleanInput($data) {
   	$data = trim($data);
@@ -122,10 +81,7 @@
   	$data = htmlspecialchars($data);
   	return $data;
   }
-	
-	// paneme ühenduse kinni
-	$mysqli->close();
-
+  
 ?>
 <!DOCTYPE html>
 <html>
