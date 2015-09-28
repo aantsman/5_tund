e0e00e85a3ff8145280aacfca96c43f3dd0d57c5
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-09-28 13:44:26 +0300

    updates

--- functions.php
+++ functions.php
@@ -1,1 +1,57 @@
-functions
+<?php
+    //loome AB ühenduse
+    /*  
+        // config_global.php
+        $servername = "";
+        $server_username = "";
+        $server_password = "";
+    
+    */
+    require_once("../config_global.php");
+    $database = "if15_romil_1";
+    
+    //paneme sessiooni serveris tööle, saaame kasutada SESSION[]
+    session_start();
+    
+    
+    function logInUser($email, $hash){
+        
+        // GLOBALS saab kätte kõik muutujad mis kasutusel
+        $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        
+        $stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+        $stmt->bind_param("ss", $email, $hash);
+        $stmt->bind_result($id_from_db, $email_from_db);
+        $stmt->execute();
+        if($stmt->fetch()){
+            echo "Kasutaja logis sisse id=".$id_from_db;
+            
+            // sessioon, salvestatakse serveris
+            $_SESSION['logged_in_user_id'] = $id_from_db;
+            $_SESSION['logged_in_user_email'] = $email_from_db;
+            
+            //suuname kasutaja teisele lehel
+            header("Location: data.php");
+            
+        }else{
+            echo "Wrong credentials!";
+        }
+        $stmt->close();
+        
+        $mysqli->close();
+        
+    }
+    
+    
+    function createUser($create_email, $hash){
+        
+        $mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        $stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?,?)");
+        $stmt->bind_param("ss", $create_email, $hash);
+        $stmt->execute();
+        $stmt->close();
+        
+        $mysqli->close();
+        
+    }
+ ?>
